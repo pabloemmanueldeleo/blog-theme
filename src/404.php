702d0ac1acb092d9a3d994820ed8c23c7e3767d5
Aysha Anggraini <aysha@viki.com>
2017-01-08 14:40:31 +0800

    Basic 404 style

--- src/404.php
+++ src/404.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
-	<main role="main" aria-label="Content">
+	<main role="main" aria-label="Content" class="content">
 		<!-- section -->
 		<section>
 
 			<!-- article -->
 			<article id="post-404">
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+				<h1 class="article-title"><?php _e( 'Page not found', 'html5blank' ); ?></h1>
 				<h2>
 					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
 				</h2>
@@ -18,7 +18,5 @@
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
