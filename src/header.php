b235b36e8ba4a5973481cb077b4daeb6a59d96b6
Aysha Anggraini <ren.aysha@gmail.com>
2017-01-09 17:16:48 +0800

    Add my own favicon

--- src/header.php
+++ src/header.php
@@ -4,6 +4,7 @@
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' : '; } ?><?php bloginfo('name'); ?> | <?php bloginfo('description'); ?></title>
 
+		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
 		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
